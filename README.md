535aac69960b3b3ec7cb815563e60df66a60e11c
IWIllPull <matas.minelga@gmail.com>
2022-06-01 18:16:24 +0300

    code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # VirusGameRust
-VirusGame in Rust
+[VirusGame](https://github.com/carykh/VirusGame/) with a spin on it, rewritten in Rust and Bevy.
+
+## Todos
+[ ] - Implement cells/walls that do nothing lol
+[ ] - Implement particle functionality
+[ ] - Implement simple food -> energy -> waste cycle with a spin on it (eat -> get enerty -> consume energy -> to get more energy, free itself up from waste)
+[ ] - Implement simple genes (OG VirusGame basis) functionality
+[ ] - Implement cell walls
+[ ] - Mutations
+[ ] - Implement codons visual representation
+[ ] - Implement UGOs
+[ ] - Implement more advanced waste/ugo removal system
+[ ] - Implement advanced genes functionality ()
+[ ] - Implement multiplication for cells
+[ ] - GUI editor for cell genes/ugos
+[ ] - Speed controls (play/pause, faster, slower)
+[ ] - Simulation settings in general
+[ ] - Stats (alive, mutations, "natural" vs "replicated")
+## Nice to haves
+[ ] - Write up what genes do
+[ ] - Graphs for stats
+[ ] - Gene stats + graphs (most common snips, gene strings)
+[ ] - More complex genes (regulate mutation rate, cell ticking rate, etc.)
+[ ] - Food production/storage by cells themselves
+[ ] - Toggle to use only original genes + functionality (maybe)
+[ ] - World exporting/importing
+[ ] - Cell/organism importing/exporting
+[ ] - Cell communication (hormones/pheromones)
+
+
+
+## Run instructions
+1. Get Rust
+2. For quicker builds get [LLD](https://github.com/carykh/VirusGame/)
+3. `cargo run` for developing, `cargo run --release` to run with all optimizations.
--- src/main.rs
+++ src/main.rs
@@ -10,64 +10,34 @@ use bevy::{
 // Defines the amount of time that should elapse between each physics step.
 const TIME_STEP: f32 = 1.0 / 60.0;
 
-// These constants are defined in `Transform` units.
-// Using the default 2D camera they correspond 1:1 with screen pixels.
-// The `const_vec3!` macros are needed as functions that operate on floats cannot be constant in Rust.
-const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 20.0, 0.0]);
-const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-const PADDLE_SPEED: f32 = 500.0;
-// How close can the paddle get to the wall
-const PADDLE_PADDING: f32 = 10.0;
+const WORLD_SIZE: f32 = 1000.0;
+const WORLD_SCALE: f32 = 1.0;
 
-// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
-const BALL_STARTING_POSITION: Vec3 = const_vec3!([0.0, -50.0, 1.0]);
-const BALL_SIZE: Vec3 = const_vec3!([30.0, 30.0, 0.0]);
-const BALL_SPEED: f32 = 400.0;
-const INITIAL_BALL_DIRECTION: Vec2 = const_vec2!([0.5, -0.5]);
+const BOUNDARY_THICKNESS: f32 = 10.0;
 
-const WALL_THICKNESS: f32 = 10.0;
-// x coordinates
-const LEFT_WALL: f32 = -450.;
-const RIGHT_WALL: f32 = 450.;
-// y coordinates
-const BOTTOM_WALL: f32 = -300.;
-const TOP_WALL: f32 = 300.;
-
-const BRICK_SIZE: Vec2 = const_vec2!([15., 15.]);
+const CELL_SIZE: Vec2 = const_vec2!([8., 8.]);
 // These values are exact
-const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 0.0;
-const GAP_BETWEEN_BRICKS: f32 = 5.0;
-// These values are lower bounds, as the number of bricks is computed
-const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
-const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
-
-const SCOREBOARD_FONT_SIZE: f32 = 40.0;
-const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+const GAP_BETWEEN_CELLS: f32 = 1.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
-const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
-const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
-const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const WALL_COLOR: Color = Color::rgb(0.2, 0.2, 0.2);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(Scoreboard { score: 0 })
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
         .add_event::<CollisionEvent>()
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(check_for_collisions)
-                .with_system(move_paddle.before(check_for_collisions))
-                .with_system(apply_velocity.before(check_for_collisions)),
+                // .with_system(check_for_collisions)
+                // .with_system(move_paddle.before(check_for_collisions))
+                // .with_system(apply_velocity.before(check_for_collisions)),
         )
-        .add_system(update_scoreboard)
         .add_system(bevy::input::system::exit_on_esc_system)
+        .add_system(energy_system)
+        .add_system(size_system)
         .run();
 }
 
@@ -75,7 +45,12 @@ fn main() {
 struct Paddle;
 
 #[derive(Component)]
-struct Ball;
+struct Particle;
+
+#[derive(Component)]
+struct Energy {
+    value: f32,
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
@@ -87,7 +62,13 @@ struct Collider;
 struct CollisionEvent;
 
 #[derive(Component)]
-struct Brick;
+struct Looptarounder;
+
+#[derive(Default)]
+struct LooptaroundEvent;
+
+#[derive(Component)]
+struct Cell;
 
 // This bundle is a collection of the components that define a "wall" in our game
 #[derive(Bundle)]
@@ -97,6 +78,7 @@ struct WallBundle {
     #[bundle]
     sprite_bundle: SpriteBundle,
     collider: Collider,
+    looptarounder: Looptarounder,
 }
 
 /// Which side of the arena is this wall located on?
@@ -107,81 +89,22 @@ enum WallLocation {
     Top,
 }
 
-enum BrickColor {
-    Red,
-    Green,
-    Blue,
+#[derive(PartialEq)]
+enum CellType {
+    Empty,
+    Wall,
+    Cell,
 }
 
-impl BrickColor {
+impl CellType {
     fn to_color(&self) -> Color {
         match self {
-            BrickColor::Red => Color::rgb(1.0, 0.0, 0.0),
-            BrickColor::Green => Color::rgb(0.0, 1.0, 0.0),
-            BrickColor::Blue => Color::rgb(0.0, 0.0, 1.0),
+            CellType::Empty => Color::rgb(1.0, 1.0, 1.0),
+            CellType::Wall => WALL_COLOR,
+            CellType::Cell => Color::rgb(0.0, 0.1, 1.0),
         }
     }
 }
-
-
-impl WallLocation {
-    fn position(&self) -> Vec2 {
-        match self {
-            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
-            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
-            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
-            WallLocation::Top => Vec2::new(0., TOP_WALL),
-        }
-    }
-
-    fn size(&self) -> Vec2 {
-        let arena_height = TOP_WALL - BOTTOM_WALL;
-        let arena_width = RIGHT_WALL - LEFT_WALL;
-        // Make sure we haven't messed up our constants
-        assert!(arena_height > 0.0);
-        assert!(arena_width > 0.0);
-
-        match self {
-            WallLocation::Left => Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS),
-            WallLocation::Right => Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS),
-            WallLocation::Bottom => Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS),
-            WallLocation::Top => Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS),
-        }
-    }
-}
-
-impl WallBundle {
-    // This "builder method" allows us to reuse logic across our wall entities,
-    // making our code easier to read and less prone to bugs when we change the logic
-    fn new(location: WallLocation) -> WallBundle {
-        WallBundle {
-            sprite_bundle: SpriteBundle {
-                transform: Transform {
-                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
-                    // This is used to determine the order of our sprites
-                    translation: location.position().extend(0.0),
-                    // The z-scale of 2D objects must always be 1.0,
-                    // or their ordering will be affected in surprising ways.
-                    // See https://github.com/bevyengine/bevy/issues/4149
-                    scale: location.size().extend(1.0),
-                    ..default()
-                },
-                sprite: Sprite {
-                    color: WALL_COLOR,
-                    ..default()
-                },
-                ..default()
-            },
-            collider: Collider,
-        }
-    }
-}
-
-// This resource tracks the game's score
-struct Scoreboard {
-    score: usize,
-}
-
 // Will check if the brick should be spawned. Used for fractal formation
 fn should_spawn_type(x: usize, y: usize, world_size: usize) -> usize {
     // array of 4 integers:
@@ -212,6 +135,22 @@ fn should_spawn_type(x: usize, y: usize, world_size: usize) -> usize {
     return result;
 }
 
+fn energy_system(mut query: Query<&mut Energy>){
+    for mut energy in query.iter_mut() {
+        energy.value -= 0.001;
+        println!("Energy: {}", energy.value);
+    }
+}
+
+// for entities with energy, we will change their size depending on their energy
+fn size_system(mut query: Query<(&Energy, &mut Transform)>) {
+    for (energy, mut transform) in query.iter_mut() {
+        let scale_x = energy.value * CELL_SIZE.x;
+        let scale_y = energy.value * CELL_SIZE.y;
+        transform.scale = Vec3::new(scale_x, scale_y, 1.0);
+    }
+}
+
 
 // Add the game's entities to our world
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -219,178 +158,86 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
 
-    // Paddle
-    let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
-
-    commands
-        .spawn()
-        .insert(Paddle)
-        .insert_bundle(SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, paddle_y, 0.0),
-                scale: PADDLE_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: PADDLE_COLOR,
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Collider);
-
-    // Ball
-    commands
-        .spawn()
-        .insert(Ball)
-        .insert_bundle(SpriteBundle {
-            transform: Transform {
-                scale: BALL_SIZE,
-                translation: BALL_STARTING_POSITION,
-                ..default()
-            },
-            sprite: Sprite {
-                color: BALL_COLOR,
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED));
-
-    // Scoreboard
-    commands.spawn_bundle(TextBundle {
-        text: Text {
-            sections: vec![
-                TextSection {
-                    value: "Score: ".to_string(),
-                    style: TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: SCOREBOARD_FONT_SIZE,
-                        color: TEXT_COLOR,
-                    },
-                },
-                TextSection {
-                    value: "".to_string(),
-                    style: TextStyle {
-                        font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-                        font_size: SCOREBOARD_FONT_SIZE,
-                        color: SCORE_COLOR,
-                    },
-                },
-            ],
-            ..default()
-        },
-        style: Style {
-            position_type: PositionType::Absolute,
-            position: Rect {
-                top: SCOREBOARD_TEXT_PADDING,
-                left: SCOREBOARD_TEXT_PADDING,
-                ..default()
-            },
-            ..default()
-        },
-        ..default()
-    });
-
-    // Walls
-    commands.spawn_bundle(WallBundle::new(WallLocation::Left));
-    commands.spawn_bundle(WallBundle::new(WallLocation::Right));
-    commands.spawn_bundle(WallBundle::new(WallLocation::Bottom));
-    commands.spawn_bundle(WallBundle::new(WallLocation::Top));
-
-    // Bricks
+    // Cells
     // Negative scales result in flipped sprites / meshes,
     // which is definitely not what we want here
-    assert!(BRICK_SIZE.x > 0.0);
-    assert!(BRICK_SIZE.y > 0.0);
-
-    let total_width_of_bricks = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
-    let bottom_edge_of_bricks = paddle_y + GAP_BETWEEN_PADDLE_AND_BRICKS;
-    let total_height_of_bricks = TOP_WALL - bottom_edge_of_bricks - GAP_BETWEEN_BRICKS_AND_CEILING;
-
-    assert!(total_width_of_bricks > 0.0);
-    assert!(total_height_of_bricks > 0.0);
+    assert!(CELL_SIZE.x > 0.0);
+    assert!(CELL_SIZE.y > 0.0);
 
     // Given the space available, compute how many rows and columns of bricks we can fit
-    let n_columns = (total_width_of_bricks / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as usize;
-    let n_rows = (total_height_of_bricks / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as usize;
+    let n_columns = (WORLD_SIZE * WORLD_SCALE / (CELL_SIZE.x + GAP_BETWEEN_CELLS)).floor() as usize;
+    let n_rows = (WORLD_SIZE * WORLD_SCALE / (CELL_SIZE.y + GAP_BETWEEN_CELLS)).floor() as usize;
     let n_vertical_gaps = n_columns - 1;
 
     // Because we need to round the number of columns,
     // the space on the top and sides of the bricks only captures a lower bound, not an exact value
-    let center_of_bricks = (LEFT_WALL + RIGHT_WALL) / 2.0;
+    let center_of_bricks = 0.0;
     let left_edge_of_bricks = center_of_bricks
         // Space taken up by the bricks
-        - (n_columns as f32 / 2.0 * BRICK_SIZE.x)
+        - (n_columns as f32 / 2.0 * CELL_SIZE.x)
         // Space taken up by the gaps
-        - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_BRICKS;
+        - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_CELLS;
 
     // In Bevy, the `translation` of an entity describes the center point,
     // not its bottom-left corner
-    let offset_x = left_edge_of_bricks + BRICK_SIZE.x / 2.;
-    let offset_y = bottom_edge_of_bricks + BRICK_SIZE.y / 2.;
+    let offset_x = CELL_SIZE.x - (WORLD_SIZE * WORLD_SCALE / 2.);
+    let offset_y = CELL_SIZE.y - WORLD_SIZE * WORLD_SCALE / 2.;
 
     for row in 0..n_rows {
         for column in 0..n_columns {
-            let brick_type = should_spawn_type(row, column, n_rows);
-            let brick_color = match brick_type {
-                0 => BrickColor::Red,
-                1 => BrickColor::Green,
-                _ => BrickColor::Blue,
+            let cell_idx = should_spawn_type(row, column, n_rows);
+            let cell_type = match cell_idx {
+                0 => CellType::Empty,
+                1 => CellType::Wall,
+                _ => CellType::Cell,
 
             };
-            let brick_position = Vec2::new(
-                offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS),
-                offset_y + row as f32 * (BRICK_SIZE.y + GAP_BETWEEN_BRICKS),
+            let cell_position = Vec2::new(
+                offset_x + column as f32 * (CELL_SIZE.x + GAP_BETWEEN_CELLS),
+                offset_y + row as f32 * (CELL_SIZE.y + GAP_BETWEEN_CELLS),
             );
                 
 
 
             // brick
-            commands
-                .spawn()
-                .insert(Brick)
-                .insert_bundle(SpriteBundle {
-                    sprite: Sprite {
-                        color: brick_color.to_color(),
+            if cell_type == CellType::Cell {
+                commands
+                    .spawn()
+                    .insert(Cell)
+                    .insert_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: cell_type.to_color(),
+                            ..default()
+                        },
+                        transform: Transform {
+                            translation: cell_position.extend(0.0),
+                            scale: Vec3::new(CELL_SIZE.x, CELL_SIZE.y, 1.0),
+                            ..default()
+                        },
                         ..default()
-                    },
-                    transform: Transform {
-                        translation: brick_position.extend(0.0),
-                        scale: Vec3::new(BRICK_SIZE.x, BRICK_SIZE.y, 1.0),
+                    })
+                    .insert(Collider)
+                    .insert(Cell)
+                    .insert(Energy{value: 1.0});
+            } else if cell_type == CellType::Wall {
+                commands
+                    .spawn()
+                    .insert_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: cell_type.to_color(),
+                            ..default()
+                        },
+                        transform: Transform {
+                            translation: cell_position.extend(0.0),
+                            scale: Vec3::new(CELL_SIZE.x, CELL_SIZE.y, 1.0),
+                            ..default()
+                        },
                         ..default()
-                    },
-                    ..default()
-                })
-                .insert(Collider);
+                    })
+                    .insert(Collider);
+            }
         }
     }
-}
-
-fn move_paddle(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Paddle>>,
-) {
-    let mut paddle_transform = query.single_mut();
-    let mut direction = 0.0;
-
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
-    }
-
-    if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
-    }
-
-    // Calculate the new horizontal paddle position based on player input
-    let new_paddle_position = paddle_transform.translation.x + direction * PADDLE_SPEED * TIME_STEP;
-
-    // Update the paddle position,
-    // making sure it doesn't cause the paddle to leave the arena
-    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
-    let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
-
-    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
 }
 
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
@@ -400,19 +247,13 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
     }
 }
 
-fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
-    let mut text = query.single_mut();
-    text.sections[1].value = format!("{}", scoreboard.score);
-}
-
 fn check_for_collisions(
     mut commands: Commands,
-    mut scoreboard: ResMut<Scoreboard>,
-    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
-    collider_query: Query<(Entity, &Transform, Option<&Brick>), With<Collider>>,
+    mut ball_query: Query<(&mut Velocity, &mut Transform), With<Particle>>,
+    collider_query: Query<(Entity, &Transform, Option<&Cell>), (With<Collider>, Without<Particle>)>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
-    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let (mut ball_velocity, mut ball_transform) = ball_query.single_mut();
     let ball_size = ball_transform.scale.truncate();
 
     // check collision with walls
@@ -427,35 +268,30 @@ fn check_for_collisions(
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
 
-            // Bricks should be despawned and increment the scoreboard on collision
-            if maybe_brick.is_some() {
-                scoreboard.score += 1;
-                commands.entity(collider_entity).despawn();
-            }
 
             // reflect the ball when it collides
-            let mut reflect_x = false;
-            let mut reflect_y = false;
+            let mut collide_x = false;
+            let mut collide_y = false;
 
             // only reflect if the ball's velocity is going in the opposite direction of the
             // collision
             match collision {
-                Collision::Left => reflect_x = ball_velocity.x > 0.0,
-                Collision::Right => reflect_x = ball_velocity.x < 0.0,
-                Collision::Top => reflect_y = ball_velocity.y < 0.0,
-                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
+                Collision::Left => collide_x = ball_velocity.x > 0.0,
+                Collision::Right => collide_x = ball_velocity.x < 0.0,
+                Collision::Top => collide_y = ball_velocity.y < 0.0,
+                Collision::Bottom => collide_y = ball_velocity.y > 0.0,
                 Collision::Inside => { /* do nothing */ }
             }
 
-            // reflect velocity on the x-axis if we hit something on the x-axis
-            if reflect_x {
+            if collide_x {
                 ball_velocity.x = -ball_velocity.x;
             }
 
             // reflect velocity on the y-axis if we hit something on the y-axis
-            if reflect_y {
+            if collide_y {
                 ball_velocity.y = -ball_velocity.y;
             }
+            
         }
     }
 }
